b846e279611a7d58cc6cd9547dc1625835fdd91a
Sang Ha Hwang <putcharmaster@gmail.com>
2025-02-04 13:27:31 +0100

    module00 ex01 corrected

--- Module00/ex01/Contact.hpp
+++ Module00/ex01/Contact.hpp
@@ -25,19 +25,13 @@ private:
 
 public:
     void setInfo(std::string first_name, std::string last_name, std::string nickname, 
-    std::string phone_number, std::string darkest_secret){
-        this->first_name = first_name;
-        this->last_name = last_name;
-        this->nickname = nickname;
-        this->phone_number = phone_number;
-        this->darkest_secret = darkest_secret;
-    }
+    std::string phone_number, std::string darkest_secret);
 
-    std::string getFirstName(){ return this->first_name; }
-    std::string getLastName(){ return this->last_name; }
-    std::string getNickname(){ return this->nickname; }
-    std::string getPhoneNumber(){ return this->phone_number; }
-    std::string getDarkestSecret(){ return this->darkest_secret; }
+    std::string getFirstName();
+    std::string getLastName();
+    std::string getNickname();
+    std::string getPhoneNumber();
+    std::string getDarkestSecret();
 };
 
 #endif
