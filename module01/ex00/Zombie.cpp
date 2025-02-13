c82defdbe28bbe3260ab6f6df051eb1140337818
Sang Ha Hwang <putcharmaster@gmail.com>
2025-02-13 16:41:23 +0100

    pushed

--- module01/ex00/Zombie.cpp
+++ module01/ex00/Zombie.cpp
@@ -10,4 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
 
+Zombie::Zombie(std::string name) : name(name){}
+
+Zombie::~Zombie(){}
